bd8f9503d31276a7492b6a003d7bf9feddb0a996
Nilovva <slavka11118@yandex.ru>
2024-01-17 22:03:09 +0400

    Добавьте файлы проекта.

---
+++ Request for proposal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+
+   
+    namespace Request_for_proposal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] sentences = text.Split(new char[] { '.', '?', '!' });
+            string[] wordsToMatch1 = Console.ReadLine().Split();
+            string[] wordsToMatch2 = Console.ReadLine().Split();
+            var sentenceQuery = from sentence in sentences
+                                let w = sentence.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' },
+                                                        StringSplitOptions.RemoveEmptyEntries)
+                                where w.Distinct().Intersect(wordsToMatch1).Count() == wordsToMatch1.Count() && w.Distinct().Intersect(wordsToMatch2).Count() == wordsToMatch2.Count()
+                                select sentence;
+
+            foreach (string str in sentenceQuery)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
